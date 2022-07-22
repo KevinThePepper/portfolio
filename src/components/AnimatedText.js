6777a55ca508c49cb3e4352bd1bbe6ada39e220d
Kevin Shelley <kevin.shelley@pm.me>
2022-07-21 21:00:43 -0700

    feat: set text index to start at 0

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -7,7 +7,7 @@ const AnimatedText = () => {
   const [text, setText] = useState(0);
   useEffect(() => {
     const interval = setInterval(() => {
-      setText(text < 2 ? text + 1 : 1);
+      setText(text < 2 ? text + 1 : 0);
     }, 5000);
     return () => clearInterval(interval);
   });
